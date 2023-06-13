a3fd2ffa9cda73716570e14da3b82fbc0dd62558
ashley.amachree@instructionpartners.org <ashley.amachree@instructionpartners.org>
2023-06-13 18:14:34 +0000

    created season and walkthrough system level

--- models/ipg_data/walkthroughcollectionlvl.sql
+++ models/ipg_data/walkthroughcollectionlvl.sql
@@ -1,3 +1,30 @@
-SELECT system, school, school_year, subject, date, visit_c, AVG(standard_alignment_c) AS standard_alignment,AVG(mastery_c) AS mastery, AVG(ca_1a_binary) AS CA_1A, AVG(ca_1b_binary) AS CA_1B, AVG(ca_1c_binary) AS CA_1C, AVG(ca_1d) AS CA_1D, 
+with av AS (SELECT system, school, school_year, subject, date, visit_c, AVG(standard_alignment_c) AS standard_alignment,AVG(mastery_c) AS mastery, AVG(ca_1a_binary) AS CA_1A, AVG(ca_1b_binary) AS CA_1B, AVG(ca_1c_binary) AS CA_1C, AVG(ca_1d) AS CA_1D, 
 AVG(ca_2_a_c) AS CA_2A, AVG(ca_2_b_c) AS CA_2B, AVG(ca_2_c_c) AS CA_2C, AVG(ca_2_d_c) AS CA_2D, AVG(ca_2_e_c) AS CA_2E, AVG(ca_2_f_c) AS CA_2F, AVG(ca_3_a_c) AS CA_3A, AVG(ca_3_b_c) AS CA_3B, AVG(ca_3_c_c) AS CA_3C, AVG(ca_3_d_c) AS CA_3D, AVG(ca_3_e_c) AS CA_3E, 
-AVG(ca_1_ppi) AS CA_1_PPI, AVG(ca2ppi) AS CA_2_PPI, AVG(ca3ppi) AS CA_3_PPI, AVG(totalppi) AS total_PPI FROM ip-ipg-data.ipg_transformations.walkthroughlvl GROUP BY 1,2,3,4,5,6
+AVG(ca_1_ppi) AS CA_1_PPI, AVG(ca2ppi) AS CA_2_PPI, AVG(ca3ppi) AS CA_3_PPI, AVG(totalppi) AS total_PPI FROM ip-ipg-data.ipg_transformations.walkthroughlvl GROUP BY 1,2,3,4,5,6), 
+
+dates AS (SELECT system, school, school_year, subject, MIN(date) AS min, MAX(date) AS max FROM av GROUP BY 1,2,3,4),
+
+withdate AS (SELECT av.*, min, max FROM av join dates ON av.system = dates.system AND av.school = dates.school AND av.school_year = dates.school_year AND av.subject = dates.subject),
+
+season AS (SELECT *, 
+CASE 
+WHEN max <= min + 42 THEN
+    CASE 
+    WHEN EXTRACT(month FROM date) >= 8 AND EXTRACT(month FROM date) <11
+    THEN "Fall"
+    WHEN EXTRACT(month FROM date) >= 11 OR EXTRACT(month FROM date) < 3
+    THEN "Winter"
+    ELSE "Spring"
+    END
+WHEN date <= min + 42
+THEN "Fall"
+WHEN date >= max - 42
+THEN "Spring"
+ELSE "Winter"
+END 
+AS season
+
+FROM withdate)
+
+SELECT system, school, school_year, subject, date, season.season, visit_c, standard_alignment, mastery, CA_1A, CA_1B, CA_1C, CA_1D, CA_2A, CA_2B, CA_2C, CA_2D, CA_2E, CA_2F, CA_3A, CA_3B, CA_3C, CA_3D, CA_3E, CA_1_PPI, CA_2_PPI, CA_3_PPI, total_PPI
+FROM season ORDER BY 1,2,3,4,6
---
+++ models/ipg_data/walkthroughsystemlvl.sql
@@ -0,0 +1,21 @@
+WITH  uniquesys AS (select distinct New_System, School FROM ip-ipg-data.dimension_table.pmdim), 
+
+
+newsys AS
+ (select dim.New_System, school.*,  from ip-ipg-data.ipg_transformations.walkthroughschoollvl AS school LEFT OUTER JOIN uniquesys dim ON school.school = dim.School),
+
+
+ 
+ 
+ cleansys AS (SELECT
+ CASE 
+ WHEN New_System IS NULL THEN system ELSE New_System END AS new_system,  newsys.* EXCEPT (New_System) FROM newsys),
+
+ av AS (SELECT new_system, system, school_year, subject, season, AVG(standard_alignment) AS standard_alignment,AVG(mastery) AS mastery, AVG(CA_1A) AS CA_1A, AVG(CA_1B) AS CA_1B, AVG(CA_1C) AS CA_1C, AVG(CA_1D) AS CA_1D, 
+AVG(CA_2A) AS CA_2A, AVG(CA_2B) AS CA_2B, AVG(CA_2C) AS CA_2C, AVG(CA_2D) AS CA_2D, AVG(CA_2E) AS CA_2E, AVG(CA_2F) AS CA_2F, AVG(CA_3A) AS CA_3A, AVG(CA_3B) AS CA_3B, AVG(CA_3C) AS CA_3C, AVG(CA_3D) AS CA_3D, AVG(CA_3E) AS CA_3E, 
+AVG(CA_1_PPI) AS CA_1_PPI, AVG(CA_2_PPI) AS CA_2_PPI, AVG(CA_3_PPI) AS CA_3_PPI, AVG(total_PPI) AS total_PPI FROM ip-ipg-data.ipg_transformations.walkthroughschoollvlb
+GROUP BY 1,2,3,4,5)
+
+SELECT * FROM av
+
+ 
