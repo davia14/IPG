14a71c54a61a8f7b43a77c47fc68e06cefb170dc
ashley.amachree@instructionpartners.org <ashley.amachree@instructionpartners.org>
2023-06-13 21:38:44 +0000

    -

--- models/ipg_data/surveysfordash.sql
+++ models/ipg_data/surveysfordash.sql
@@ -3,6 +3,6 @@ WITH addnames AS (SELECT surv.*, dim.name AS staff_name, dim.cluster FROM ip-ipg
 emails AS (SELECT addnames.*, e.email_for_access, e.category FROM addnames LEFT OUTER JOIN ip-ipg-data.dimension_table.email_list AS e ON addnames.staff_name = e.staff_member_),
 
 withraw AS (SELECT emails.*, submitter_first_name_c, submitter_last_name_c, submitters_email_c, response FROM emails JOIN ip-ipg-data.ipg_transformations.surveylvl AS surv ON emails.new_system = surv.new_system 
-AND emails.school_year=surv.school_year_c AND emails.survey_type = surv.survey_type, emails.question = surv.question)
+AND emails.school_year=surv.school_year_c AND emails.survey_type = surv.survey_type AND emails.question = surv.question)
 
 SELECT * FROM withraw 
